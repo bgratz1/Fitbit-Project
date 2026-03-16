87ca1387bea735a574f2b274459f0b94157041eb
Benny Eisner <b.a.eisner@wustl.edu>
2026-03-16 15:43:56 +0100

    add (feat): add merged dataframe with mostly cleaned entries

--- part_4.py
+++ part_4.py
@@ -46,21 +46,22 @@ heart_rate = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
 
 # Fill missing weight values by using the median weight
 weight_log["WeightKg"] = weight_log["WeightKg"].fillna(weight_log["WeightKg"].median())
-weight_log["WeightPounds"] = weight_log["WeightPounds"].fillna(
-    weight_log["WeightPounds"].median()
-)
+weight_log.drop(
+    columns=["WeightPounds"], inplace=True
+)  # can easily convert Kg to Pounds so dont need to take up the extra space
 
 # Dropping the 'Fat' column since over 90% of the data is missing
 weight_log.drop(columns=["Fat"], inplace=True)
 
 # Standardizing date
-daily_activity["data"] = pd.to_datetime(daily_activity["ActivityDate"]).dt.date
+daily_activity["date"] = pd.to_datetime(daily_activity["ActivityDate"]).dt.date
 weight_log["date"] = pd.to_datetime(weight_log["Date"]).dt.date
 
 hourly_calories["date"] = pd.to_datetime(hourly_calories["ActivityHour"]).dt.date
 hourly_intensity["date"] = pd.to_datetime(hourly_intensity["ActivityHour"]).dt.date
 hourly_steps["date"] = pd.to_datetime(hourly_steps["ActivityHour"]).dt.date
 heart_rate["date"] = pd.to_datetime(heart_rate["Time"]).dt.date
+minute_sleep["date"] = pd.to_datetime(minute_sleep["date"]).dt.date
 
 # Aggregating the sub daily tables to daily values
 daily_calories = hourly_calories.groupby(["Id", "date"])["Calories"].sum().reset_index()
@@ -76,3 +77,37 @@ daily_sleep.rename(columns={"value": "MinutesAsleep"}, inplace=True)
 
 daily_hr = heart_rate.groupby(["Id", "date"])["Value"].mean().reset_index()
 daily_hr.rename(columns={"Value": "AvgHeartRate"}, inplace=True)
+
+
+# Merging into daily_activity
+merged = daily_activity.copy()
+
+merged = merged.merge(
+    weight_log[["Id", "date", "WeightKg", "BMI"]], on=["Id", "date"], how="left"
+)
+merged = merged.merge(daily_calories, on=["Id", "date"], how="left")
+merged = merged.merge(daily_intensity, on=["Id", "date"], how="left")
+merged = merged.merge(daily_steps_hourly, on=["Id", "date"], how="left")
+merged = merged.merge(daily_sleep, on=["Id", "date"], how="left")
+merged = merged.merge(daily_hr, on=["Id", "date"], how="left")
+
+merged.drop(columns=["Calories_y"], inplace=True)
+merged.rename(columns={"Calories_x": "Calories"}, inplace=True)
+
+# Drop sparse rows with missing crucial data
+merged = merged[merged["Calories"] > 0]
+merged.drop(
+    columns=["LoggedActivitiesDistance", "SedentaryActiveDistance"], inplace=True
+)
+
+merged["WeightKg"] = merged.groupby("Id")["WeightKg"].ffill().bfill()
+merged["BMI"] = merged.groupby("Id")["BMI"].ffill().bfill()
+
+# Add column to bin sleep quality
+merged["HoursAsleep"] = merged["MinutesAsleep"] / 60
+merged["SleepCategory"] = pd.cut(
+    merged["HoursAsleep"],
+    bins=[0, 6, 9, 24],
+    labels=["Under-slept", "Normal", "Over-slept"],
+)
+print(merged.head(50))
